96fb349e5dbc9e81c8712245c6e431c6025beef4
ocrensys <ocrensys@gmail.com>
2022-11-25 10:12:24 -0600

    payment type CRUD

--- src/api/payment-type/dto/create-payment-type.dto.ts
+++ src/api/payment-type/dto/create-payment-type.dto.ts
@@ -1,1 +1,12 @@
-export class CreatePaymentTypeDto {}
+import { IsBoolean, IsString, MaxLength } from 'class-validator';
+import * as NUMBER from '../../../common/constants/number.contants';
+export class CreatePaymentTypeDto {
+  @IsString()
+  @MaxLength(NUMBER.N100, {
+    message: `The Payment Type's name must contain at least ${NUMBER.N100}`,
+  })
+  name: string;
+
+  @IsBoolean()
+  isActive: boolean;
+}
