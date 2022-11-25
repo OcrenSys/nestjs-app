bdc5558af903f9d4f6aaf34e6e47fa3a07dafb14
ocrensys <ocrensys@gmail.com>
2022-11-25 11:41:34 -0600

    role CRUD

--- src/api/role/dto/create-role.dto.ts
+++ src/api/role/dto/create-role.dto.ts
@@ -1,1 +1,10 @@
-export class CreateRoleDto {}
+import { IsBoolean, IsString } from 'class-validator';
+import { RoleEnum } from 'src/common/enums/roles.enum';
+
+export class CreateRoleDto {
+  @IsString()
+  name: RoleEnum;
+
+  @IsBoolean()
+  isActive: boolean;
+}
--- src/api/role/role.service.ts
+++ src/api/role/role.service.ts
@@ -1,26 +1,213 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpStatus,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import {
+  ACTION_CREATE,
+  MODEL,
+  ACTION_FIND,
+  ONLY_ONE,
+  ACTION_UPDATE,
+  ACTION_REMOVE,
+} from 'src/common/constants/messages.constants';
+import { QueryRunner, Repository, DataSource } from 'typeorm';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
+import { Role } from './entities/role.entity';
 
 @Injectable()
 export class RoleService {
-  create(createRoleDto: CreateRoleDto) {
-    return 'This action adds a new role';
+  private readonly logger: Logger = new Logger('RoleService');
+  private queryRunner: QueryRunner;
+
+  constructor(
+    @InjectRepository(Role)
+    private readonly roleRepository: Repository<Role>,
+    private readonly dataSource: DataSource,
+  ) {}
+
+  async create(createRoleDto: CreateRoleDto) {
+    const { ...toCreate } = createRoleDto;
+
+    this.queryRunner = this.dataSource.createQueryRunner();
+    await this.queryRunner.connect();
+    await this.queryRunner.startTransaction();
+
+    try {
+      const role = await this.roleRepository.create(toCreate);
+
+      this.roleRepository.save(role);
+
+      await this.queryRunner.commitTransaction();
+
+      return {
+        data: role,
+        statusCode: HttpStatus.CREATED,
+        message: ACTION_CREATE.success(MODEL.Role),
+      };
+    } catch (error) {
+      this.queryRunner.rollbackTransaction();
+      throw new BadRequestException(
+        {
+          data: error,
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: ACTION_CREATE.error(MODEL.Role),
+        },
+        ACTION_CREATE.error(MODEL.Role),
+      );
+    } finally {
+      this.queryRunner.release();
+    }
   }
 
-  findAll() {
-    return `This action returns all role`;
+  async findAll() {
+    const filters = {};
+    const relations = [];
+
+    try {
+      const roles = await this.roleRepository.find({
+        where: filters,
+        relations,
+      });
+
+      return {
+        data: roles,
+        statusCode: HttpStatus.OK,
+        message: ACTION_FIND.success(MODEL.Role),
+      };
+    } catch (error) {
+      this.logger.error(error?.message, error);
+      throw new NotFoundException(
+        {
+          data: error,
+          status: HttpStatus.NOT_FOUND,
+          message: ACTION_FIND.error(MODEL.Role),
+        },
+        ACTION_FIND.error(MODEL.Role),
+      );
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} role`;
+  async findOne(id: number) {
+    const filters = { id };
+    const relations = [];
+
+    try {
+      const role: Role = await this.roleRepository.findOne({
+        relations,
+        where: filters,
+      });
+
+      if (!role)
+        return new NotFoundException(
+          {
+            data: null,
+            status: HttpStatus.NOT_FOUND,
+            message: ACTION_FIND.error(MODEL.Role, ONLY_ONE),
+          },
+          ACTION_FIND.error(MODEL.Role, ONLY_ONE),
+        );
+
+      return {
+        data: role,
+        statusCode: HttpStatus.OK,
+        message: ACTION_FIND.success(MODEL.Role, ONLY_ONE),
+      };
+    } catch (error) {
+      throw new InternalServerErrorException(
+        {
+          data: error,
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: ACTION_FIND.error(MODEL.Role, ONLY_ONE),
+        },
+        ACTION_FIND.error(MODEL.Role),
+      );
+    }
   }
 
-  update(id: number, updateRoleDto: UpdateRoleDto) {
-    return `This action updates a #${id} role`;
+  async update(id: number, updateRoleDto: UpdateRoleDto) {
+    const { ...toUpdate } = updateRoleDto;
+
+    this.queryRunner = this.dataSource.createQueryRunner();
+    await this.queryRunner.connect();
+    await this.queryRunner.startTransaction();
+
+    const role: Role = await this.roleRepository.preload({
+      id,
+      ...toUpdate,
+    });
+
+    if (!role)
+      return new NotFoundException(
+        {
+          data: null,
+          status: HttpStatus.NOT_FOUND,
+          message: ACTION_UPDATE.error(MODEL.Role),
+        },
+        ACTION_UPDATE.error(MODEL.Role),
+      );
+
+    try {
+      this.roleRepository.save(role);
+
+      await this.queryRunner.commitTransaction();
+
+      return {
+        data: role,
+        statusCode: HttpStatus.OK,
+        message: ACTION_UPDATE.success(MODEL.Role),
+      };
+    } catch (error) {
+      await this.queryRunner.rollbackTransaction();
+      throw new InternalServerErrorException(
+        {
+          data: error,
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: ACTION_UPDATE.error(MODEL.Role),
+        },
+        ACTION_UPDATE.error(MODEL.Role),
+      );
+    } finally {
+      await this.queryRunner.release();
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} role`;
+  async remove(id: number) {
+    const role = await this.roleRepository.findOne({
+      where: { id },
+    });
+
+    if (!role)
+      return new NotFoundException(
+        {
+          data: null,
+          status: HttpStatus.NOT_FOUND,
+          message: ACTION_UPDATE.error(MODEL.Role),
+        },
+        ACTION_REMOVE.error(MODEL.Role),
+      );
+    try {
+      const result = await this.roleRepository.delete(id);
+
+      return {
+        data: result,
+        statusCode: HttpStatus.OK,
+        message: ACTION_REMOVE.success(MODEL.Role),
+      };
+    } catch (error) {
+      throw new InternalServerErrorException(
+        {
+          data: error,
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: ACTION_REMOVE.error(MODEL.Role),
+        },
+        ACTION_REMOVE.error(MODEL.Role),
+      );
+    }
   }
 }
