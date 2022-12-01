a0fd219ad428d46d555f9a0eb12bee3206100a3c
Josue Lopez <josue.ismael.lopez.ramirez@gmail.com>
2022-12-01 12:18:47 -0600

    update docker file

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config, options);
   SwaggerModule.setup('api/v1', app, document);
-  await app.listen(3000);
+  await app.listen(parseInt(process.env.API_PORT_DEV, 10) || 3000);
 }
 bootstrap();
