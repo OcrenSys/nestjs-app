cc4aba9e775753a38bda8128d9ca99c7d38c5229
JAIRO MARTINEZ <ocrensys@gmail.com>
2022-12-12 18:34:48 -0600

    improve migrations run

--- src/config/typeorm.config-migrations.ts
+++ src/config/typeorm.config-migrations.ts
@@ -2,11 +2,21 @@ import { DataSource } from 'typeorm';
 
 export default new DataSource({
   type: 'mysql',
-  host: 'mysql',
-  port: 3306,
-  username: 'root',
-  database: 'inventory-db',
-  password: '12345678',
+  /**   RUNNING IN COMMAND LINE  */
+  /*  
+    host: 'localhost',
+    username: 'smbs',
+    database: 'inventory',
+    password: '12345678',
+    port: 3306, 
+  */
+  /**   RUNNING IN DOCKER COMPOSE  */
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE_NAME,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  port: parseInt(process.env.DATABASE_PORT, 10),
+
   entities: [`${__dirname}/../**/*.entity.{js,ts}`],
   migrations: [`${__dirname}/../database/migrations/*{.ts,.js}`],
   synchronize: false,
